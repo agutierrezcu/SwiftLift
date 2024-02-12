c9617d1821874269104c921de876a24f67cf7107
armando <agutierrezcu@gmail.com>
2024-02-12 09:24:15 +0100

    Add Build event enricher tests

--- src/SwiftLift.Infrastructure/BuildInfo/BuildEventEnricher.cs
+++ src/SwiftLift.Infrastructure/BuildInfo/BuildEventEnricher.cs
@@ -6,17 +6,17 @@ namespace SwiftLift.Infrastructure.BuildInfo;
 internal sealed class BuildEventEnricher(IServiceProvider _serviceProvider)
     : ILogEventEnricher
 {
-    private Lazy<List<LogEventProperty>>? _cachedBuildProperties;
+    private static Lazy<List<LogEventProperty>>? s_cachedBuildProperties;
 
     public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
     {
         Guard.Against.Null(logEvent);
         Guard.Against.Null(propertyFactory);
 
-        _cachedBuildProperties ??= new Lazy<List<LogEventProperty>>(
+        s_cachedBuildProperties ??= new Lazy<List<LogEventProperty>>(
             () => CreateEventProperties(propertyFactory).ToList());
 
-        foreach (var property in _cachedBuildProperties.Value)
+        foreach (var property in s_cachedBuildProperties.Value)
         {
             logEvent.AddPropertyIfAbsent(property);
         }
---
+++ tests/SwiftLift.Infrastructure.UnitTests/BuildInfo/BuildEventEnricherTests.cs
@@ -0,0 +1,176 @@
+using Serilog.Core;
+using Serilog.Events;
+using SwiftLift.Infrastructure.BuildInfo;
+using Xunit.Extensions.Ordering;
+
+namespace SwiftLift.Infrastructure.UnitTests.BuildInfo;
+
+public sealed class BuildEventEnricherTests
+{
+    [Fact]
+    public void Given_NullLogEvent_When_Enrich_Then_ShouldThrowException()
+    {
+        // Arrange
+        var serviceProvider = Substitute.For<IServiceProvider>();
+
+        var sut = new BuildEventEnricher(serviceProvider);
+
+        // Act
+        Action act = () => sut.Enrich(null, null);
+
+        // Assert
+        act.Should()
+            .Throw<ArgumentNullException>()
+            .WithMessage("Value cannot be null. (Parameter 'logEvent')");
+    }
+
+    [Fact]
+    public void Given_NullPropertyFactory_When_Enrich_Then_ShouldThrowException()
+    {
+        // Arrange
+        var logEvent = new LogEvent(DateTimeOffset.Now, LogEventLevel.Information, null, new MessageTemplate(string.Empty, []), []);
+
+        var serviceProvider = Substitute.For<IServiceProvider>();
+
+        var sut = new BuildEventEnricher(serviceProvider);
+
+        //Act
+        Action act = () => sut.Enrich(logEvent, null);
+
+        // Assert
+        act.Should()
+            .Throw<ArgumentNullException>()
+            .WithMessage("Value cannot be null. (Parameter 'propertyFactory')");
+    }
+
+    [Fact, Order(1)]
+    public void Given_LogEvent_When_Enrich_Then_BuildPropertiesAreAdded()
+    {
+        // Arrange
+        var build = new Build
+        {
+            Id = "1",
+            Number = "1.0.0",
+            Branch = "master",
+            Commit = "abc123",
+            Url = "http://localhost"
+        };
+
+        var propertyFactory = Substitute.For<ILogEventPropertyFactory>();
+
+        propertyFactory.CreateProperty(Arg.Any<string>(), Arg.Any<object?>())
+           .Returns(callInfo => new LogEventProperty((string)callInfo[0], new ScalarValue(callInfo[1])));
+
+        var buildManager = Substitute.For<IBuildManager>();
+
+        buildManager.GetBuildAsync(default).Returns(Task.FromResult(build));
+
+        var serviceProvider = Substitute.For<IServiceProvider>();
+
+        serviceProvider.GetService(typeof(IBuildManager)).Returns(buildManager);
+
+        var logEvent = new LogEvent(DateTimeOffset.Now, LogEventLevel.Information, null,
+            new MessageTemplate(string.Empty, []), []);
+
+        var sut = new BuildEventEnricher(serviceProvider);
+
+        // Act
+        sut.Enrich(logEvent, propertyFactory);
+
+        // Assert
+        serviceProvider
+            .Received(1)
+            .GetService(typeof(IBuildManager));
+
+        buildManager
+            .Received(1)
+            .GetBuildAsync(default);
+
+        propertyFactory
+            .Received(3)
+            .CreateProperty(Arg.Any<string>(), Arg.Any<object?>());
+
+        propertyFactory
+           .Received(1)
+           .CreateProperty("BuildId", build.Id);
+
+        propertyFactory
+          .Received(1)
+          .CreateProperty("BuildNumber", build.Number);
+
+        propertyFactory
+          .Received(1)
+          .CreateProperty("BuildCommit", build.Commit);
+
+        var properties = logEvent.Properties;
+
+        var expectedProperties = new List<LogEventProperty>
+        {
+            new("BuildId", new ScalarValue(build.Id)),
+            new("BuildNumber", new ScalarValue(build.Number)),
+            new("BuildCommit", new ScalarValue(build.Commit))
+        };
+
+        foreach (var expectedProperty in expectedProperties)
+        {
+            properties.Should().ContainKey(expectedProperty.Name);
+            properties[expectedProperty.Name].Should().Be(expectedProperty.Value);
+        }
+    }
+
+    [Fact, Order(2)]
+    public void Given_LogEvent_When_Enrich_Then_BuildPropertiesAreAdded_FromCache()
+    {
+        // Arrange
+        var build = new Build
+        {
+            Id = "1",
+            Number = "1.0.0",
+            Branch = "master",
+            Commit = "abc123",
+            Url = "http://localhost"
+        };
+
+        var propertyFactory = Substitute.For<ILogEventPropertyFactory>();
+
+        var buildManager = Substitute.For<IBuildManager>();
+
+        var serviceProvider = Substitute.For<IServiceProvider>();
+
+        var logEvent = new LogEvent(DateTimeOffset.Now, LogEventLevel.Information, null,
+            new MessageTemplate(string.Empty, []), []);
+
+        var sut = new BuildEventEnricher(serviceProvider);
+
+        // Act
+        sut.Enrich(logEvent, propertyFactory);
+
+        // Assert
+        serviceProvider
+            .Received(0)
+            .GetService(typeof(IBuildManager));
+
+        buildManager
+            .Received(0)
+            .GetBuildAsync(default);
+
+        propertyFactory
+            .Received(0)
+            .CreateProperty(Arg.Any<string>(), Arg.Any<object?>());
+
+        var properties = logEvent.Properties;
+
+        var expectedProperties = new List<LogEventProperty>
+        {
+            new("BuildId", new ScalarValue(build.Id)),
+            new("BuildNumber", new ScalarValue(build.Number)),
+            new("BuildCommit", new ScalarValue(build.Commit))
+        };
+
+        foreach (var expectedProperty in expectedProperties)
+        {
+            properties.Should().ContainKey(expectedProperty.Name);
+            properties[expectedProperty.Name].Should().Be(expectedProperty.Value);
+        }
+    }
+}
